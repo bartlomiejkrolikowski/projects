fd9d61718772331a39fed9a31567d6cc8c2a63f7
bartlomiejkrolikowski <57042602+bartlomiejkrolikowski@users.noreply.github.com>
2022-06-12 03:06:54 +0200

    extended proofs

--- myBinTree_benchmark.v
+++ myBinTree_benchmark.v
@@ -1,6 +1,6 @@
 (* Bartłomiej Królikowski *)
 
-Load "C:/Coq/Programy/Moje/myBinTree".
+Load "myBinTree". (* path to myBintree.v*)
 
 Fixpoint insert_t (A : Type) (t : nat) (ord : A -> A -> bool) (a : A) (T : BinTree A) :
   BinTree A * nat :=
@@ -87,7 +87,7 @@ match T with
            then let Tt := remove' A T
                 in  (fst Tt, 1 + snd Tt)
            else let Tt := delete_t A (S t) ord a Tl
-                in  (Node A a' (fst Tt) Tr, snd Tt)
+                in  (Node A a' (fst Tt) Tr, 1 + snd Tt)
     else let Tt := delete_t A (S t) ord a Tr
          in  (Node A a' Tl (fst Tt), snd Tt)
   | Empty _ => (Empty A, t)
@@ -275,4 +275,208 @@ Theorem insert'_complex :
     snd (insert' A ord a T) <= 1 + height A T.
 Proof.
   unfold insert'. intros. apply insert_t_complex.
+Qed.
+
+Lemma find_t_complex :
+  forall (A : Type) (ord : A -> A -> bool) (a : A) (T : BinTree A) (t : nat),
+    snd (find_t A t ord a T) <= t + height A T.
+Proof.
+  induction T; intros; cbn.
+  - destruct (ord a a0).
+   + destruct (ord a0 a).
+     * cbn. apply Nat.le_add_r.
+     * apply Nat.le_trans with (S t + height A T1).
+       -- apply IHT1.
+       -- rewrite Nat.add_succ_r. rewrite <- Nat.add_succ_l. apply Nat.add_le_mono_l.
+          apply Nat.le_max_l.
+   + apply Nat.le_trans with (S t + height A T2).
+     * apply IHT2.
+     * rewrite Nat.add_succ_r. rewrite <- Nat.add_succ_l. apply Nat.add_le_mono_l.
+       apply Nat.le_max_r.
+  - intuition.
+Qed.
+
+Theorem find'_complex :
+  forall (A : Type) (ord : A -> A -> bool) (a : A) (T : BinTree A),
+    snd (find' A ord a T) <= 1 + height A T.
+Proof.
+  unfold find'. intros. apply find_t_complex.
+Qed.
+
+Lemma min_t_complex :
+  forall (A : Type) (T : BinTree A) (t : nat),
+    snd (min_t A t T) <= t + height A T.
+Proof.
+  induction T; intros; cbn.
+  - destruct T1.
+    + apply Nat.le_trans with (S t + height A (Node A a0 T1_1 T1_2)).
+      * apply IHT1.
+      * rewrite Nat.add_succ_r. rewrite <- Nat.add_succ_l. apply Nat.add_le_mono_l.
+        apply Nat.le_max_l.
+    + cbn. rewrite Nat.add_succ_r. intuition.
+  - intuition.
+Qed.
+
+Theorem min'_complex :
+  forall (A : Type) (T : BinTree A),
+    snd (min' A T) <= 1 + height A T.
+Proof.
+  unfold min'. intros. apply min_t_complex.
+Qed.
+
+Lemma max_t_complex :
+  forall (A : Type) (T : BinTree A) (t : nat),
+    snd (max_t A t T) <= t + height A T.
+Proof.
+  induction T; intros; cbn.
+  - destruct T2.
+    + apply Nat.le_trans with (S t + height A (Node A a0 T2_1 T2_2)).
+      * apply IHT2.
+      * rewrite Nat.add_succ_r. rewrite <- Nat.add_succ_l. apply Nat.add_le_mono_l.
+        apply Nat.le_max_r.
+    + cbn. rewrite Nat.add_succ_r. intuition.
+  - intuition.
+Qed.
+
+Theorem max'_complex :
+  forall (A : Type) (T : BinTree A),
+    snd (max' A T) <= 1 + height A T.
+Proof.
+  unfold max'. intros. apply max_t_complex.
+Qed.
+
+Lemma removeMax_t_complex :
+  forall (A : Type) (T : BinTree A) (t : nat),
+    snd (removeMax_t A t T) <= t + height A T.
+Proof.
+  induction T; intros; cbn.
+  - destruct T2.
+    + apply Nat.le_trans with (S t + height A (Node A a0 T2_1 T2_2)).
+      * unfold snd. fold (snd (removeMax_t A (S t) (Node A a0 T2_1 T2_2))). apply IHT2.
+      * rewrite Nat.add_succ_r. rewrite <- Nat.add_succ_l. apply Nat.add_le_mono_l.
+        apply Nat.le_max_r.
+    + cbn. rewrite Nat.add_succ_r. intuition.
+  - intuition.
+Qed.
+
+Theorem removeMax'_complex :
+  forall (A : Type) (T : BinTree A),
+    snd (removeMax' A T) <= 1 + height A T.
+Proof.
+  unfold removeMax'. intros. apply removeMax_t_complex.
+Qed.
+
+Theorem root'_complex :
+  forall (A : Type) (T : BinTree A),
+    snd (root' A T) = 1.
+Proof.
+  destruct T; trivial.
+Qed.
+
+Theorem remove'_complex :
+  forall (A : Type) (T : BinTree A),
+    snd (remove' A T) <= 1 + 2 * height A T.
+Proof.
+  destruct T; cbn.
+  - assert (forall (A B : Type) (p : A * B), p = (fst p, snd p)).
+    + destruct p. trivial.
+    + rewrite H with (Maybe A) nat (max' A T1). destruct (fst (max' A T1)).
+      * cbn. apply le_n_S. rewrite <-Nat.add_succ_l. apply Nat.add_le_mono.
+        -- apply Nat.le_trans with (1 + height A T1).
+          ++ apply max'_complex.
+          ++ cbn. apply le_n_S, Nat.le_max_l.
+        -- apply Nat.le_trans with (1 + height A T1).
+          ++ apply removeMax'_complex.
+          ++ cbn. apply le_n_S. rewrite Nat.add_0_r. apply Nat.le_max_l.
+      * cbn. apply le_n_S. rewrite <-Nat.add_succ_l. apply le_plus_trans.
+        apply le_trans with (1 + height A T1).
+        -- apply max'_complex.
+        -- apply le_n_S, Nat.le_max_l.
+  - trivial.
+Qed.
+
+Lemma delete_t_complex :
+  forall (A : Type) (ord : A -> A -> bool) (a : A) (T : BinTree A) (t : nat),
+    snd (delete_t A t ord a T) <= t + 2 * (1 + height A T).
+Proof.
+  induction T; intros; cbn.
+  - destruct (ord a a0); cbn.
+    + destruct (ord a0 a); cbn.
+      * rewrite Nat.add_comm. apply le_plus_trans.
+        assert (forall (A B : Type) (p : A * B), p = (fst p, snd p)).
+        -- destruct p. trivial.
+        -- rewrite H with (Maybe A) nat (max' A T1). destruct (fst (max' A T1)).
+          ++ cbn. apply le_n_S, le_n_S. rewrite Nat.add_succ_r, <-Nat.add_succ_l.
+             apply Nat.add_le_mono.
+            ** apply Nat.le_trans with (1 + height A T1).
+              --- apply max'_complex.
+              --- cbn. apply le_n_S, Nat.le_max_l.
+            ** apply Nat.le_trans with (1 + height A T1).
+              --- apply removeMax'_complex.
+              --- cbn. apply le_n_S. rewrite Nat.add_0_r. apply Nat.le_max_l.
+          ++ cbn. apply le_n_S, le_n_S. rewrite Nat.add_succ_r, <-Nat.add_succ_l.
+             apply le_plus_trans, Nat.le_trans with (1 + height A T1).
+             ** apply max'_complex.
+             ** apply le_n_S, Nat.le_max_l.
+      * rewrite Nat.add_succ_r, Nat.add_succ_r. apply le_n_S.
+        apply Nat.le_trans with (S t + 2 * (1 + height A T1)).
+        -- apply IHT1.
+        -- cbn. apply le_n_S, Nat.add_le_mono_l.
+           rewrite Nat.add_succ_r, Nat.add_succ_r, Nat.add_succ_r. apply le_n_S, le_n_S.
+           apply Nat.add_le_mono.
+          ++ apply Nat.le_max_l.
+          ++ apply Nat.add_le_mono_r, Nat.le_max_l.
+    + apply Nat.le_trans with (S t + 2 * (1 + height A T2)).
+      * apply IHT2.
+      * cbn. rewrite <-Nat.add_succ_r, <-Nat.add_succ_r.
+        apply Nat.add_le_mono_l, le_S, le_n_S, Nat.add_le_mono.
+        -- apply Nat.le_max_r.
+        -- apply le_n_S, le_n_S, Nat.add_le_mono_r, Nat.le_max_r.
+  - intuition.
+Qed.
+
+Theorem delete'_complex :
+  forall (A : Type) (ord : A -> A -> bool) (a : A) (T : BinTree A),
+    snd (delete' A ord a T) <= 1 + 2 * (1 + height A T).
+Proof.
+  unfold delete'. intros. apply delete_t_complex.
+Qed.
+
+Fact pow2_positive :
+  forall n : nat, 1 <= 2 ^ n.
+Proof.
+  induction n; cbn.
+  - trivial.
+  - intuition.
+Qed.
+
+Theorem size'_complex :
+  forall (A : Type) (T : BinTree A),
+    snd (size' A T) <= 2 ^ (1 + height A T) - 1.
+Proof.
+  induction T; cbn.
+  - apply Nat.le_trans with (S (2 ^ (1 + height A T1) + 2 ^ (1 + height A T2) - 2)).
+    + apply le_n_S.
+      rewrite <-Nat.add_1_l, Nat.sub_add_distr, Nat.add_1_l, <-Nat.add_sub_assoc.
+      rewrite Nat.add_sub_swap. apply Nat.add_le_mono; assumption.
+      1-2: apply pow2_positive.
+    + rewrite <-Nat.sub_succ_l.
+      * cbn. apply Nat.sub_le_mono_r. apply Nat.add_le_mono.
+        -- apply Nat.add_le_mono.
+          ++ apply Nat.pow_le_mono_r.
+            ** intro. inversion H.
+            ** apply Nat.le_max_l.
+          ++ apply Nat.add_le_mono_r, Nat.pow_le_mono_r.
+            ** intro. inversion H.
+            ** apply Nat.le_max_l.
+        -- rewrite Nat.add_0_r, Nat.add_0_r, Nat.add_0_r. apply Nat.add_le_mono.
+          ++ apply Nat.pow_le_mono_r.
+            ** intro. inversion H.
+            ** apply Nat.le_max_r.
+          ++ apply Nat.pow_le_mono_r.
+            ** intro. inversion H.
+            ** apply Nat.le_max_r.
+      * rewrite <-Nat.add_1_l.
+        apply Nat.add_le_mono; rewrite Nat.add_1_r; apply pow2_positive.
+  - trivial.
 Qed.
